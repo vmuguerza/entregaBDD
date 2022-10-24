4905767bc1999a42101e872f6969fd813f804bb1
Victoria Muguerza <victoriamuguerza@MacBook-Air-de-Victoria.local>
2022-10-24 19:01:34 -0300

    add .py

---
+++ Cliente.py
@@ -0,0 +1,9 @@
+class Cliente:
+    def __init__(self, nombre, mail, direccion, telefono):
+        self._nombre = nombre
+        self._mail= mail
+        self._direccion=direccion
+        self._telefono=telefono
+    
+    
+        
