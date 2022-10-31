bff2b3a4263abfc4910e3062e770475c521b7d20
vmuguerza <vmuguerza@correo.um.edu.uy>
2022-10-31 14:34:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,36 @@ def menu():
      if opcion == 1:
           dar_de_alta()
      if opcion == 2:
+          dar_de_baja()
      if opcion == 3:
+          modificar()
      if opcion == 4:
+          ingresar_pedido()
      if opcion == 5:
+          ingresar_stock()
      if opcion == 6:  
+          registrar_pago()
+     else:
+          opcion = input('ingrese el numero:')
+
 
 def dar_de_alta():
-     
+     return null
+
+def dar_de_baja():
+     return null   
+
+def modificar():
+     return null   
+
+def ingresar_pedido():
+     return null  
+
+def ingresar_stock():
+     return null  
+
+def registrar_pago():
+     return null  
 
 if __name__ =="__main__":
      pg_db = PostgresqlDatabase('dbd2', user='dbd2g1', password='dbd2#G1',
