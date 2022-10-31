33bc5955333c0d8d8b38c5d8233faf7d78dc1906
vmuguerza <vmuguerza@correo.um.edu.uy>
2022-10-31 19:40:20 -0300

    changes

--- Cliente.py
+++ Cliente.py
@@ -13,67 +13,86 @@ class Cliente:
         self._apto = apto
         self._cod_postal = cod_postal
 
-    
+    @property
     def get_nombre(self):
         return self._nombre
-
+    @property
     def set_nombre(self, nuevo):
         self._nombre = nuevo
 
+    @property
     def get_apellido(self):
         return self._apellido
 
+    @property
     def set_apellido(self, nuevo):
         self._apellido = nuevo
 
+    @property
     def get_dni(self):
         return self._dni
 
+    @property
     def get_mail(self):
         return self._mail
 
+    @property
     def set_mail(self, nuevo):
         self._mail = nuevo
 
+    @property
     def get_telefono(self):
         return self._telefono
-
+    
+    @property
     def set_telefono(self, nuevo):
         self._telefono = nuevo
 
+    @property
     def get_depto(self):
         return self._depto
 
+    @property
     def set_depto(self, nuevo):
         self._depto = nuevo
 
+    @property
     def get_localidad(self):
         return self._localidad
 
+    @property
     def set_localidad(self, nuevo):
         self._localidad = nuevo
 
+    @property
     def get_calle(self):
         return self._calle
 
+    @property
     def set_calle(self, nuevo):
         self._calle = nuevo
 
+    @property
     def get_num_puerta(self):
         return self._num_puerta
 
+    @property
     def set_num_puerta(self, nuevo):
         self._num_puerta = nuevo
 
+    @property
     def get_apto(self):
         return self._apto
 
+    @property
     def set_apto(self, nuevo):
         self._apto = nuevo
 
+    @property
     def get_cod_postal(self):
         return self._cod_postal
 
+    @property
     def set_cod_postal(self, nuevo):
         self._cod_postal = nuevo
 
--- Funciones.py
+++ Funciones.py
@@ -1,4 +1,4 @@
-from Cliente import Cliente
+import Cliente
 
 
 def dar_de_alta():
@@ -15,23 +15,56 @@ def dar_de_alta():
      depto= input('ingrese el departamento:')
      cod_postal = input('ingrese el codigo postal:')
      nuevo=Cliente(nombre,apellido,dni,mail,depto,localidad,calle,nro_puerta,apto,cod_postal,telefono)
+     return
 
 
 
 def dar_de_baja():
-     return null   
+     print('Dar de baja cliente:')
+     din=input('ingrese el dni del cliente:')
+     return None
 
 def modificar():
-     return null   
+     cliente=input('Ingrese el DNI del cliente que desea modificar')
+     opcion=input('Ingrese que datos del cliente desea modificar')
+     
+     
+     if opcion==1:
+          mail=input('ingrese el mail:')
+          cleinte.set_mail(mail)
+     if opcion==2:
+          telefono=input('ingrese el telefono:')
+          cliente.set_telefono(telefono)
+
+     if opcion==3:
+          calle=input('ingrese la calle:')
+          nro_puerta=input('ingrese el numero de puerta:')
+          apto=input('ingrese el apartamento (si corresponde):')
+          localidad = input('ingrese la localidad:')
+          depto= input('ingrese el departamento:')
+          cod_postal = input('ingrese el codigo postal:')
+          cliente.set_calle(calle)
+          cliente.set_calle(nro_puerta)
+          cliente.set_calle(apto)
+          cliente.set_calle(localidad)
+          cliente.set_calle(depto)
+          cliente.set_calle(cod_postal)
+          
+     return
 
 def ingresar_pedido():
-     return null  
+     print('Ingresar pedido')
+     producto=input('')
+     cantidad=input('')
+     nuevo_pedido=PedidoSimple(producto,cantidad,'aplicacion',None,cliente)
+     print('pedido confirmado, el precio total es:',nuevo_pedido.get_precio_total)
+     return None  
 
 def ingresar_stock():
-     return null  
+     return None  
 
 def registrar_pago():
-     return null  
+     return None  
 
 def error(): 
     print("\nUno o más datos ingresados son inválidos, intente nuevamente\n")
